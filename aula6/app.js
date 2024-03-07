71cf99aef16372fee3377eb95de0a3abd1293ec1
lalmendra <lucascontatoprincipal@gmail.com>
2024-03-07 19:21:29 -0300

    update

---
+++ aula6/app.js
@@ -0,0 +1,14 @@
+let contador = 1
+
+console.log(contador++);
+console.log(contador);
+
+const num1 = 10;
+const num2 = '5';
+
+console.log(num1 * num2)
+console.log(num1 + num2)
+console.log(typeof(num1 + num2))
+console.log(num1 + parseInt(num2))
+console.log(num1 + parseInt('5.2'))
+console.log(num1 + parseFloat('5.2'))
